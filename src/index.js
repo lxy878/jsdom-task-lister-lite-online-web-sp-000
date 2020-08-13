aaf154819157310f765c7078591e7cf9beb38007
Xiaoyi Liu <lxy878@gmail.com>
2020-08-13 07:59:42 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ document.addEventListener("DOMContentLoaded", () => {
 document.addEventListener('submit', (event)=>{
   // console.log(event.target);
   description_list.push(description.value);
-  listTask(list);
+  list.innerHTML = listTask(list);
   event.preventDefault();
 });
 
