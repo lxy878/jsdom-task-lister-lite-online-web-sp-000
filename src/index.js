f1edd74cad5605928fca4c43220d545636b016b8
Xiaoyi Liu <lxy878@gmail.com>
2020-08-13 08:05:24 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
-const tasks = document.querySelector('ul#tasks');
-const description = document.querySelector('input#new-task-description');
-const descriptions = [];
-
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
 
-document.addEventListener('submit', (event)=>{
-  // console.log(event.target);
-  descriptions.push(description.value);
-  tasks.innerHTML = listTask();
-  event.preventDefault();
+  const tasks = document.querySelector('ul#tasks');
+  const description = document.querySelector('input#new-task-description');
+  const descriptions = [];
+    
+  document.addEventListener('submit', (event)=>{
+    // console.log(event.target);
+    descriptions.push(description.value);
+    tasks.innerHTML = () =>{
+      let descriptions = '';
+      for(let description of descriptions){
+        descriptions += `<li>${description}</li>`;
+      }
+      return descriptions
+    }
+    event.preventDefault();
+  });
+  
 });
 
 function listTask(){
