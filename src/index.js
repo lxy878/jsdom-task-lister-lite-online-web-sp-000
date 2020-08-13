be8f4df160fbf744c0d00e3611841fbe2ed66734
Xiaoyi Liu <lxy878@gmail.com>
2020-08-13 08:41:20 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+  const tasks = document.querySelector('ul#tasks');
+  const description = document.querySelector('input#new-task-description');
+  const descriptions = [];
+
+  document.addEventListener('submit', (event)=>{
+    descriptions.push(description.value);
+    tasks.innerHTML = listTask(descriptions);
+    event.target.reset();
+    event.preventDefault();
+  });
+
+  document.addEventListener('click', (event)=>{
+    if(event.target.nodeName === 'BUTTON'){
+      const id = event.target.description_id;
+      descriptions.splice(id, 1);
+      tasks.innerHTML = listTask(descriptions);
+    }
+  });
 });
+
+function listTask(descriptions){
+  let list = '';
+  for(let i=0; i<descriptions.length; i++){
+    list += `<li>${descriptions[i]}<button description_id='${i}'>X</button></li>`;
+  }
+  return list;
+}
