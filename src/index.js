b17a229f4f18736676560713ce00a52235c3d82e
Xiaoyi Liu <lxy878@gmail.com>
2020-08-13 07:49:04 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -9,7 +9,6 @@ document.addEventListener("DOMContentLoaded", () => {
 document.addEventListener('click', (event)=>{
   // console.log(event.target);
   const description = document.querySelector('input#new-task-description');
-  console.log(description.value);
   description_list.push(description.value);
   event.preventDefault();
 });
